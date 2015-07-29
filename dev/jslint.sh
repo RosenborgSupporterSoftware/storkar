b3b738fee5cd901831a753e2c93e365c3a419cf5
Lars J. Aas <lars.j.aas@gmail.com>
2015-07-29 18:39:22 +0200

    move presto config to storkar-dev.conf

--- dev/jslint.sh
+++ dev/jslint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for scriptfile in `find webapps/storkar -name "*.js"`; do
+for scriptfile in `find webapps/storkar-dev -name "*.js"`; do
     echo $scriptfile;
     jslint -nologo -nofilelisting -nosummary -process $scriptfile;
 done
--- storkar.sh
+++ storkar.sh
@@ -2,5 +2,17 @@
 
 source setup.sh --from-script
 
-./src/presto/src/main/boot.scm $@
+configfile=storkar-dev.conf
+for arg in "$@"; do
+  case "$arg" in
+  "--profile="*)
+    configfile="storkar-"`echo -- $arg | cut -d= -f2-`".conf"
+    ;;
+  esac
+done
+
+if test x"$configfile" != x""; then
+  set -- --config "$configfile" "$@"
+fi
+./src/presto/src/main/boot.scm "$@"
 
